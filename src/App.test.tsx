a4fd880d569a599b3412d37fe6847a1db04e9dfb
kooku <kooku@kakao.com>
2021-10-17 23:15:05 +0900

    Revert "create-react-app을 이용해 프로젝트 셋팅 (#3)"
    This reverts commit 46f99aa5acabd942f10fd6a627df090f879a704e.

--- src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders Beans', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Beans/i);
-  expect(linkElement).toBeInTheDocument();
-});
