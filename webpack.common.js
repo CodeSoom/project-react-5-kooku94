3bcb2e5cba337fbf4cad6d3b500e0e664a765338
kooku <kooku@kakao.com>
2021-10-18 02:50:40 +0900

    Add react-refresh-webpack-plugin

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,8 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 const path = require('path');
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
 
 module.exports = {
   entry: {
@@ -10,7 +13,14 @@ module.exports = {
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        use: 'babel-loader',
+        use: [
+          {
+            loader: require.resolve('babel-loader'),
+            options: {
+              plugins: [isDevelopment && require.resolve('react-refresh/babel')].filter(Boolean),
+            },
+          },
+        ],
       },
     ],
   },
@@ -22,7 +32,8 @@ module.exports = {
       template: './public/index.html',
       filename: './index.html',
     }),
-  ],
+    isDevelopment && new ReactRefreshWebpackPlugin(),
+  ].filter(Boolean),
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,6 @@
 /* eslint-disable import/extensions */
 const { merge } = require('webpack-merge');
+
 const path = require('path');
 
 const common = require('./webpack.common.js');
