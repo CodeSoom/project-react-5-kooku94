0f9fc1a3e15aeb1b90635792807b7ffa383b0a68
kooku <kooku@kakao.com>
2021-10-18 20:42:03 +0900

    Revert "Add react-refresh-webpack-plugin (#15)"
    This reverts commit a57162e00cdbacf238f762ea96b5f41603a8e7f8.

--- webpack.common.js
+++ webpack.common.js
@@ -1,8 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 const path = require('path');
-
-const isDevelopment = process.env.NODE_ENV !== 'production';
 
 module.exports = {
   entry: {
@@ -13,14 +10,7 @@ module.exports = {
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        use: [
-          {
-            loader: require.resolve('babel-loader'),
-            options: {
-              plugins: [isDevelopment && require.resolve('react-refresh/babel')].filter(Boolean),
-            },
-          },
-        ],
+        use: 'babel-loader',
       },
     ],
   },
@@ -32,8 +22,7 @@ module.exports = {
       template: './public/index.html',
       filename: './index.html',
     }),
-    isDevelopment && new ReactRefreshWebpackPlugin(),
-  ].filter(Boolean),
+  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,5 @@
 /* eslint-disable import/extensions */
 const { merge } = require('webpack-merge');
-
 const path = require('path');
 
 const common = require('./webpack.common.js');
